af94e22922cb72e52bf475680e743c477295414c
uzie <PerfectoMobile256@gmail.com>
2015-02-12 10:31:37 -0500

    tests

---
+++ test/smokeTest/test2.java
@@ -0,0 +1,15 @@
+package smokeTest;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+  @Test
+  public void a() {
+	  System.out.println("Test2 A");
+  }
+  @Test
+  public void B() {
+	  System.out.println("Test2 B");
+
+  }
+}
